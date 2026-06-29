80c83268967a56abfe31c3bbb2e17d49236f6ceb
Oleksii Hilenko <aleksey.gilenko@gmail.com>
2026-06-29 20:10:48 +0300

    feat(search): implement location-search service functionality and API integration

--- src/app/core/services/location-search.ts
+++ src/app/core/services/location-search.ts
@@ -1,4 +1,32 @@
-import { Service } from '@angular/core';
+import { Service, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map, catchError, of, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+
+import { environment } from '@environments/environment';
+import { Location, GeocodingApiDto } from '@shared/models/location.model';
 
 @Service()
-export class LocationSearch {}
+export class LocationSearch {
+    private readonly http = inject(HttpClient);
+    private readonly geocodingApiUrl = environment.geocodingApiUrl;
+
+    public locationAutocomplete(query$: Observable<string>): Observable<Location[]> {
+        return query$.pipe(
+            debounceTime(500),
+            distinctUntilChanged(),
+            switchMap(query => 
+                query.trim()
+                    ? this.fetchLocations(query)
+                    : of([])
+            )
+        );
+    }
+
+    private fetchLocations(query: string): Observable<Location[]> {
+        return this.http.get<GeocodingApiDto>(`${this.geocodingApiUrl}?name=${query}&count=5&language=en&format=json`)
+            .pipe(
+                map(payload => payload.results ?? []),
+                catchError(() => of([]))
+            );
+    }
+}
