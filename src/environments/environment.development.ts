d14e4e4deb6e2fa5e464a350979f0790b70f7105
Oleksii Hilenko <aleksey.gilenko@gmail.com>
2026-06-29 20:07:05 +0300

    feat(env): add development and production configuration files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    geocodingApiUrl: 'https://geocoding-api.open-meteo.com/v1/search',
+    openMetioApiUrl: 'https://api.open-meteo.com/v1/forecast',
+};
